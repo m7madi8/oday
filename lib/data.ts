37dd5d307d8044c18df4351942cbf0ec954f19ab
m7madi8 <132115698+m7madi8@users.noreply.github.com>
2026-05-23 23:16:20 +0300

    added data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1142,7 +1142,8 @@ export const contact = {
   items: [
     {
       label: "Location",
-      value: "Ramallah, Palestine",
+      value: "Al-Bireh, Ramallah",
+      href: "https://www.google.com/maps/search/?api=1&query=31.9230623,35.2090546",
     },
     {
       label: "Email",
@@ -1160,14 +1161,15 @@ export const contact = {
 export const studioLocation = {
   eyebrow: "Studio Location",
   heading: "Where We Build",
-  headingAccent: "From Ramallah",
-  addressLine2: "Ramallah, Palestine",
-  coordinates: { lat: 31.9038, lng: 35.2034 },
+  headingAccent: "From Al-Bireh",
+  addressLine2: "Al-Bireh, Ramallah",
+  coordinates: { lat: 31.9230623, lng: 35.2090546 },
   mapEmbedUrl:
-    "https://www.openstreetmap.org/export/embed.html?bbox=35.185%2C31.892%2C35.225%2C31.918&layer=mapnik&marker=31.9038%2C35.2034",
+    "https://www.openstreetmap.org/export/embed.html?bbox=35.189%2C31.908%2C35.229%2C31.938&layer=mapnik&marker=31.9230623%2C35.2090546",
   directionsUrl:
-    "https://www.google.com/maps/dir/?api=1&destination=31.9038%2C35.2034&travelmode=driving",
-  openInMapsUrl: "https://www.google.com/maps/search/?api=1&query=Ramallah%2C+Palestine",
+    "https://www.google.com/maps/dir/?api=1&destination=31.9230623%2C35.2090546&travelmode=driving",
+  openInMapsUrl:
+    "https://www.google.com/maps/place/Dental+Spa+Clinic+%D8%A7%D9%84%D8%AF%D9%83%D8%AA%D9%88%D8%B1+%D9%85%D8%AD%D9%81%D9%88%D8%B8+%D9%81%D9%88%D8%A7%D9%84%D8%AD%D8%A9,+Al-Bireh,+Ramallah/@31.9230623,35.2090546,17z",
 };
 
 export const footer = {
